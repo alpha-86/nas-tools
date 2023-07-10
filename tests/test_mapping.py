4724a463fc555842a48003768ba4286b2e4b0079
Chao Chen <alpha.1986@gmail.com>
2023-07-10 20:26:51 +0800

    add test

---
+++ tests/test_mapping.py
@@ -0,0 +1,6 @@
+from app.media.meta import MetaInfo
+
+title = "The Long Season 2017 2160p WEB-DL H265 AAC-XXX"
+meta_info = MetaInfo(title=title)
+print("cn_name:",meta_info.cn_name)
+print("en_name:",meta_info.en_name)
