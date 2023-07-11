d66fe594d69a04d2073b910ae1caa0ee72ca73f1
Chao Chen <alpha.1986@gmail.com>
2023-07-11 16:46:01 +0800

    test_mapping

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -1,6 +1,12 @@
 from app.media.meta import MetaInfo
+import os,sys
 
 title = "The Long Season 2017 2160p WEB-DL H265 AAC-XXX"
 meta_info = MetaInfo(title=title)
 print("cn_name:",meta_info.cn_name)
-print("en_name:",meta_info.en_name)
+print("raw_name[%s]en_name[%s]"%(title, meta_info.en_name))
+#os.system("touch /config/video_name_mapping.yaml")
+os.system("cp /config/video_name_mapping.yaml2 /config/video_name_mapping.yaml")
+title = "The Long Season2 2017 2160p WEB-DL H265 AAC-XXX"
+meta_info = MetaInfo(title=title)
+print("raw_name[%s]en_name[%s]"%(title, meta_info.en_name))
